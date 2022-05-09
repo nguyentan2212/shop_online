608c03b24227f9ce0e3a4aaa48483f2137c41919
nguyentan2212 <nguyentan2212@gmail.com>
2022-05-09 21:12:15 +0700

    first commit

---
+++ ShopOnline.Api/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Repositories;
+using ShopOnline.Api.Repositories.Contracts;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContextPool<ShopOnlineDbContext>(options =>
+     // options.UseSqlServer(builder.Configuration.GetConnectionString("ShopOnlineConnection"))
+     options.UseSqlite(builder.Configuration.GetConnectionString("ShopOnlineConnectionSqlite"))
+
+);
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(policy =>
+    policy.WithOrigins("http://localhost:7060", "https://localhost:7060")
+    .AllowAnyMethod()
+    .WithHeaders(HeaderNames.ContentType)
+);
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
